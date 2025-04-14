34e329d147557bc33cd87253770605641053aa1e
Jorbon <ben.a.phillips@outlook.com>
2025-04-14 01:29:35 -0500

    added files

---
+++ sound/GeneratedSoundbanks/Wwise_IDs.h
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_GUN2_EQUIP = 336962148U;
+        static const AkUniqueID PLAYER_GUN2_SHOOT = 2888700573U;
+        static const AkUniqueID PLAYER_GUN2_STOW = 1068874439U;
+        static const AkUniqueID PLAYER_GUN_EQUIP = 4048185180U;
+        static const AkUniqueID PLAYER_GUN_SHOOT = 3655591093U;
+        static const AkUniqueID PLAYER_GUN_STOW = 2963257007U;
+        static const AkUniqueID PLAYER_KNIFE_EQUIP = 2994582105U;
+        static const AkUniqueID PLAYER_KNIFE_STOW = 3299203624U;
+        static const AkUniqueID PLAYER_KNIFE_SWING = 1408038267U;
+        static const AkUniqueID PLAYER_STEP = 3461836331U;
+        static const AkUniqueID PLAYER_STEP_MATERIAL_CRUNCH = 671048679U;
+        static const AkUniqueID PLAYER_STEP_MATERIAL_GRASS = 251429118U;
+        static const AkUniqueID PLAYER_STEP_MATERIAL_HOLLOW = 1844484457U;
+        static const AkUniqueID PLAYER_STEP_MATERIAL_NORMAL = 2509951041U;
+        static const AkUniqueID PLAYER_STEP_MATERIAL_WOOD = 72724935U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_STEP_MATERIAL
+        {
+            static const AkUniqueID GROUP = 402384277U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CRUNCH = 990217086U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID HOLLOW = 815678804U;
+                static const AkUniqueID NORMAL = 1160234136U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace PLAYER_STEP_MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUND = 623086306U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
